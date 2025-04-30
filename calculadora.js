b5f59e33dcdbba6197af334aff68724ae19926c6
ricnef2121 <ricnef2121@gmail.com>
2025-04-29 18:58:23 -0600

    Agregar multiplicación y división

--- calculadora.js
+++ calculadora.js
@@ -9,3 +9,12 @@ function restar(a, b) {
 function cuadrado(a) {
     return a * a;
 }
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    if (b === 0) throw new Error("División por cero");
+    return a / b;
+}
